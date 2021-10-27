6023b450cc8939a83aa64022ef72878f4c7f6254
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-27 10:57:10 +0700

    video 5 - pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -15,31 +15,53 @@ const Planets = () => {
   /* useQuery([key,param],async function to data,config)
   setelah fetch data diawal, untuk selanjutnya akan di cache ketika dipanggil
   */
-  const { data, isLoading, isError, isSuccess } = useQuery(
+  // const { data, isLoading, isError, isSuccess } = useQuery(
+  //   ["planets", page],
+  //   () => fetchPlanets(page),
+  //   {
+  //     staleTime: 0,
+  //     // cacheTime: 10,
+  //     onSuccess: () => console.log("Data fetched with no problem"),
+  //   }
+  // );
+
+  const { data, isError, isSuccess, isFetching, isPreviousData } = useQuery(
     ["planets", page],
     () => fetchPlanets(page),
     {
-      staleTime: 0,
-      // cacheTime: 10,
-      onSuccess: () => console.log("Data fetched with no problem"),
+      keepPreviousData: true,
     }
   );
 
   return (
     <div>
       <h2>Planets</h2>
-      <button onClick={() => setPage(1)}> Page 1</button>
-      <button onClick={() => setPage(2)}> Page 2</button>
-      <button onClick={() => setPage(3)}> Page 3</button>
 
-      {isLoading && <div>Loading data ...</div>}
+      {isFetching && <div>Loading data ...</div>}
       {isError && <div>Error fetching data</div>}
       {isSuccess && (
-        <div>
-          {data.results.map((planet, i) => (
-            <Planet planet={planet} key={i} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 0))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page} </span>
+          <button
+            onClick={() => {
+              setPage((old) => (data?.next ? old + 1 : old));
+            }}
+            disabled={isPreviousData || !data?.next}
+          >
+            Next page
+          </button>
+          <div>
+            {data.results.map((planet, i) => (
+              <Planet planet={planet} key={i} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );

613396a56ef3bf0106a7388228e41f41bde463d9
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-10-27 10:32:38 +0700

    video 4 - query variable

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
 // API = https://swapi.dev/documentation
 
-const fetchPlanets = async () => {
-  return await (await fetch("http://swapi.dev/api/planets")).json();
+const fetchPlanets = async (page) => {
+  return await (
+    await fetch(`http://swapi.dev/api/planets/?page=${page}`)
+  ).json();
 };
 
 const Planets = () => {
-  /* useQuery(key,async function to data,config)
+  const [page, setPage] = useState(1);
+  /* useQuery([key,param],async function to data,config)
   setelah fetch data diawal, untuk selanjutnya akan di cache ketika dipanggil
   */
   const { data, isLoading, isError, isSuccess } = useQuery(
-    "planets",
-    fetchPlanets,
+    ["planets", page],
+    () => fetchPlanets(page),
     {
       staleTime: 0,
       // cacheTime: 10,
       onSuccess: () => console.log("Data fetched with no problem"),
     }
   );
-  console.log(data);
+
   return (
     <div>
       <h2>Planets</h2>
-      {/* <p> {status} </p> */}
+      <button onClick={() => setPage(1)}> Page 1</button>
+      <button onClick={() => setPage(2)}> Page 2</button>
+      <button onClick={() => setPage(3)}> Page 3</button>
 
       {isLoading && <div>Loading data ...</div>}
       {isError && <div>Error fetching data</div>}
